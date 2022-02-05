f304695a0e78c1b9e3bbb0c34e3c83e034c18471
sholq <semyon.silkin@gmail.com>
2022-02-05 20:00:02 +0300

    fix: fix calculation ratio

--- scripts/index.js
+++ scripts/index.js
@@ -54,7 +54,7 @@ function renderInputLine(evt) {
 function calculateTaxes() {
   const currentNetIncomeValue = inputNetIncome.value.replace(/\D/g, '');
 
-  const grossIncome = Math.round(currentNetIncomeValue / .55);
+  const grossIncome = Math.round(currentNetIncomeValue / .57);
   const taxesMonth = Math.round(grossIncome - currentNetIncomeValue);
   const taxesYear = Math.round(taxesMonth * 12);
   const taxesFiveYear = Math.round(taxesYear * 5);

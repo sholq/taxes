af0e180696f8bb84bcc0e4e26822fe32a8c2a4b5
sholq <semyon.silkin@gmail.com>
2022-05-04 14:18:56 +0300

    refactor: delete unnecessary

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 import {Route, Switch} from "react-router-dom";
-import '../App.css';
+import './App.css';
 
 import Header from "./Header";
 import Main from "./Main";

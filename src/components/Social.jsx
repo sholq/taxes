5c08ac1915c5a1340fd0a03ef2b8bef059b200d8
sholq <semyon.silkin@gmail.com>
2022-05-15 18:00:51 +0300

    refactor: add method map to ul at Social and Taxes

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,27 +1,16 @@
 import {memo} from "react";
 
 const Social = memo(() =>{
+  const icons = ["social__link_to_facebook", "social__link_to_vkontakte", "social__link_to_instagram", "social__link_to_youtube", "social__link_to_telegram", "social__link_to_tiktok"]
+
   return (
     <section className="social main__social" aria-label="Секция репостов в социальные сети">
       <ul className="social__list">
-        <li className="social__item">
-          <a className="social__link social__link_to_facebook" href="javascript:void(0)" />
-        </li>
-        <li className="social__item">
-          <a className="social__link social__link_to_vkontakte" href="javascript:void(0)" />
-        </li>
-        <li className="social__item">
-          <a className="social__link social__link_to_instagram" href="javascript:void(0)" />
-        </li>
-        <li className="social__item">
-          <a className="social__link social__link_to_youtube" href="javascript:void(0)" />
-        </li>
-        <li className="social__item">
-          <a className="social__link social__link_to_telegram" href="javascript:void(0)" />
-        </li>
-        <li className="social__item">
-          <a className="social__link social__link_to_tiktok" href="javascript:void(0)" />
-        </li>
+        {icons.map((icon, id) => (
+          <li className="social__item" key={id}>
+            <a className={`social__link ${icon}`} href="javascript:void(0)" />
+          </li>
+        ))}
       </ul>
     </section>
   )
--- src/components/Taxes.jsx
+++ src/components/Taxes.jsx
@@ -4,27 +4,31 @@ import {memo} from "react";
 const Taxes = memo(props => {
   const {taxesForMonth, taxesForYear, taxesForFiveYear} = props;
 
+  const outputs = [{
+    header: "Налоги и сборы за месяц",
+    name: "taxes-month",
+    value: taxesForMonth,
+  }, {
+    header: "За год",
+    name: "taxes-year",
+    value: taxesForYear,
+  }, {
+    header: "И за пять лет",
+    name: "taxes-five-year",
+    value: taxesForFiveYear,
+  }];
+
   return (
     <section className="taxes main__taxes">
       <ul className="taxes__list">
-        <li className="container">
-          <h2 className="container__title container__title_taxes">Налоги и сборы за месяц</h2>
-          <label className="output output_place_taxes">
-            <Output name="taxes-month" value={taxesForMonth}/>
-          </label>
-        </li>
-        <li className="container">
-          <h2 className="container__title container__title_taxes">За год</h2>
-          <label className="output output_place_taxes">
-            <Output name="taxes-year" value={taxesForYear}/>
-          </label>
-        </li>
-        <li className="container">
-          <h2 className="container__title container__title_taxes">И за пять лет</h2>
-          <label className="output output_place_taxes">
-            <Output name="taxes-five-year" value={taxesForFiveYear}/>
-          </label>
-        </li>
+        {outputs.map(({header, name, value}, id) => (
+          <li className="container" key={id}>
+            <h2 className="container__title container__title_taxes">{header}</h2>
+            <label className="output output_place_taxes">
+              <Output name={name} value={value}/>
+            </label>
+          </li>
+        ))}
       </ul>
     </section>
   )

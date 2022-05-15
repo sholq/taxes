527115e5b15b6006023ccefb7ddd8ad58120a43d
sholq <semyon.silkin@gmail.com>
2022-05-15 18:56:36 +0300

    refactor: refactor Income and Input, unlock fuctional keys on input

--- src/components/Income.jsx
+++ src/components/Income.jsx
@@ -5,67 +5,13 @@ import Input from "./Input";
 const Income = memo(props => {
   const {setNetIncome, netIncome, grossIncome} = props;
 
-  function inputOnChange(evt) {
-    const income = evt.target.value.replace(/\D/g, '');
-    setNetIncome(income);
-  }
-
-  function renderNumber(value) {
-    return Number(value.replace(/\D/g, '')).toLocaleString('nb-NO');
-  }
-
-  function lockNaNKey(evt) {
-    if (Number.isNaN(Number(evt.key)) && evt.key !== 'Backspace') {
-      evt.preventDefault();
-    }
-  }
-  function fixMaxStringLength(evt) {
-    if (evt.target.value.length === 7 && evt.key !== 'Backspace') {
-      evt.preventDefault();
-    }
-  }
-  function handleNaNError(evt) {
-    if (Number.isNaN(Number(evt.target.value.replace(/\D/g, '') + evt.key)) && evt.key !== 'Backspace') {
-      evt.target.value = renderNumber(evt.target.value);
-    }
-  }
-
-  function handleKeyDown(evt) {
-    lockNaNKey(evt);
-    fixMaxStringLength(evt);
-    handleNaNError(evt);
-  }
-
-  function preventStickyPressKeys(evt) {
-    if (evt.target.value.length > 6) {
-      evt.target.value = evt.target.value.replace(/\D/g, '').slice(0, 6);
-    }
-  }
-  function renderInputLine(evt) {
-    if (!evt.target.value) {
-      evt.target.classList.remove('input__line_active');
-    } else {
-      evt.target.value = renderNumber(evt.target.value);
-    }
-  }
-
-  function handleKeyUp(evt) {
-    preventStickyPressKeys(evt);
-    renderInputLine(evt);
-  }
-
   return (
     <section className="income main__income">
       <ul className="income__list">
         <li className="container">
-          <h2 className="container__title container__title_income">Ваша «чистая» зарплата — то, что приходит на
-            карточку</h2>
+          <h2 className="container__title container__title_income">Ваша «чистая» зарплата — то, что приходит на карточку</h2>
           <label className="input input_place_income">
-            <Input
-              value={renderNumber(netIncome + '')}
-              onKeyDown={handleKeyDown}
-              onKeyUp={handleKeyUp}
-              onChange={inputOnChange}
+            <Input netIncome={netIncome} setNetIncome={setNetIncome}
             />
           </label>
         </li>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,60 @@
 import {memo} from "react";
 
 const Input = memo(props => {
-  const {value, onKeyDown, onKeyUp, onChange} = props;
+  const {netIncome, setNetIncome} = props;
+
+  function handleOnChange(evt) {
+    const income = evt.target.value.replace(/\D/g, '');
+    setNetIncome(income);
+  }
+
+  function renderNumber(value) {
+    return Number(value.replace(/\D/g, '')).toLocaleString('nb-NO');
+  }
+
+  function lockNaNKey(evt) {
+    if (Number.isNaN(Number(evt.key)) && evt.key !== 'Backspace') {
+      if (!(evt.keyCode < 124 && evt.keyCode > 112)) {
+        evt.preventDefault();
+      }
+    }
+  }
+  function fixMaxStringLength(evt) {
+    if (evt.target.value.length === 7 && evt.key !== 'Backspace') {
+      if (!(evt.keyCode < 124 && evt.keyCode > 112)) {
+        evt.preventDefault();
+      }
+    }
+  }
+  function handleNaNError(evt) {
+    if (Number.isNaN(Number(evt.target.value.replace(/\D/g, '') + evt.key)) && evt.key !== 'Backspace') {
+      evt.target.value = renderNumber(evt.target.value);
+    }
+  }
+
+  function handleKeyDown(evt) {
+    lockNaNKey(evt);
+    fixMaxStringLength(evt);
+    handleNaNError(evt);
+  }
+
+  function preventStickyPressKeys(evt) {
+    if (evt.target.value.length > 6) {
+      evt.target.value = evt.target.value.replace(/\D/g, '').slice(0, 6);
+    }
+  }
+  function renderInputLine(evt) {
+    if (!evt.target.value) {
+      evt.target.classList.remove('input__line_active');
+    } else {
+      evt.target.value = renderNumber(evt.target.value);
+    }
+  }
+
+  function handleKeyUp(evt) {
+    preventStickyPressKeys(evt);
+    renderInputLine(evt);
+  }
 
   return (
     <>
@@ -11,10 +64,10 @@ const Input = memo(props => {
         name="net-income"
         placeholder="30 000"
         autoComplete="off"
-        value={value}
-        onKeyDown={onKeyDown}
-        onKeyUp={onKeyUp}
-        onChange={onChange}
+        value={renderNumber(String(netIncome))}
+        onKeyDown={handleKeyDown}
+        onKeyUp={handleKeyUp}
+        onChange={handleOnChange}
       />₽
     </>
   )
